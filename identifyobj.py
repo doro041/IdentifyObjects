654de9ccd06c36c4b65ce18e95042ffd4ffa4fa0
Fraser <ftlg987@gmail.com>
2023-08-30 16:35:13 +0100

    algorithm: now detects objects and draws a box around them

--- identifyobj.py
+++ identifyobj.py
@@ -2,6 +2,8 @@
 # dependecies: Pillow (Python module)
 from PIL import Image, ImageDraw
 
+Sensitivity = 20
+
 # Open the images
 orig = Image.open("tux.png").convert("RGBA")
 changed = Image.open("tux2.png").convert("RGBA")
@@ -21,12 +23,80 @@ def get_img_diff(img0: Image, img1: Image):
             res_draw.point([(w, h)], (abs(r1 - r0), abs(g1 - g0), abs(b1 - b0), 255))
     return img_res
 
-res = get_img_diff(orig, changed)
+# checks if a given pixel should be added and if so does
+def check_pixel(obj, img_data, w, h, found, check_pixels):
+    r, g, b, _ = img_data[w, h]
+    if (r > Sensitivity or g > Sensitivity or b > Sensitivity) and (w, h) not in found:
+        found[(w, h)] = True
+        obj.append((w, h))
+        check_pixels.append((w, h))
 
-# close input images
+# look for one pixel that isn't black and then look for all its neighbours and declare an object
+def find_objects(diff_img: Image):
+    data = diff_img.load()
+    found = {} # all the pixels which have been found and shouldn't be used for searching for an object
+    res = []
+    for w in range(diff_img.width):
+        for h in range(diff_img.height):
+            r, g, b, _ = data[w, h]
+            # found an object
+            if (r > Sensitivity or g > Sensitivity or b > Sensitivity) and (w, h) not in found:
+                found[(w, h)] = True # add to found pixels
+                res.append([(w, h)])
+                obj = res[len(res) - 1]
+                check_pixels = []
+                check_pixels.append((w, h))
+                # find all neghbours of object
+                while len(check_pixels) > 0:
+                    check_w, check_h = check_pixels.pop(0)
+                    if check_w > 0:
+                        if check_h > 0:
+                            check_pixel(obj, data, check_w - 1, check_h - 1, found, check_pixels)
+                        if check_h < diff_img.height - 1:
+                            check_pixel(obj, data, check_w - 1, check_h + 1, found, check_pixels)
+                        check_pixel(obj, data, check_w - 1, check_h, found, check_pixels)
+                    if check_w < diff_img.width - 1:
+                        if check_h > 0:
+                            check_pixel(obj, data, check_w + 1, check_h - 1, found, check_pixels)
+                        if check_h < diff_img.height - 1:
+                            check_pixel(obj, data, check_w + 1, check_h + 1, found, check_pixels)
+                        check_pixel(obj, data, check_w + 1, check_h, found, check_pixels)
+                    if check_h > 0:
+                        check_pixel(obj, data, check_w, check_h - 1, found, check_pixels)
+                    if check_h < diff_img.height - 1:
+                        check_pixel(obj, data, check_w, check_h + 1, found, check_pixels)
+    return res
+
+
+
+diff_img = get_img_diff(orig, changed)
+
+# close images we don't need anymore
 orig.close()
-changed.close()
+
+objs = find_objects(diff_img)
+
+diff_img.close()
+
+diff_draw = ImageDraw.Draw(changed)
+
+for obj in objs:
+    w1 = 0
+    h1 = 0
+    w0 = diff_img.width
+    h0 = diff_img.height
+    for pixel in obj:
+        w, h = pixel
+        if w < w0:
+            w0 = w
+        if w > w1:
+            w1 = w
+        if h < h0:
+            h0 = h
+        if h > h1:
+            h1 = h
+    diff_draw.line([(w0, h0), (w1, h0), (w1, h1), (w0, h1), (w0, h0)], (0, 0, 255, 255))
 
 # show the image and close
-res.show()
-res.close()
+changed.show()
+changed.close()
