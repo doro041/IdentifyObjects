4d1fff5c38c35e2a5d49d5786401b51d1e77b150
Dolorious <doroteyastoyanova555@gmail.com>
2023-09-11 16:53:24 +0100

    added placeholder

--- registrypage.py
+++ registrypage.py
@@ -7,6 +7,26 @@ win = None
 name_entry = None
 email_entry = None
 
+# Function to handle the click event in the Name Entry
+def name_entry_click(event):
+    if name_entry.get() == "Name":
+        name_entry.delete(0, tk.END)  # Clear the placeholder text
+
+# Function to handle the click event in the Email Entry
+def email_entry_click(event):
+    if email_entry.get() == "Email":
+        email_entry.delete(0, tk.END)  # Clear the placeholder text
+
+# Function to handle the focus out event in the Name Entry
+def name_entry_focus_out(event):
+    if not name_entry.get():
+        name_entry.insert(0, "Name")  # Restore the placeholder text
+
+# Function to handle the focus out event in the Email Entry
+def email_entry_focus_out(event):
+    if not email_entry.get():
+        email_entry.insert(0, "Email")  # Restore the placeholder tex
+
 def register_user():
     # Get the user-entered data
     name = name_entry.get()
@@ -39,18 +59,23 @@ def run(root):
     logo_image = common.logo
     logo_label = tk.Label(frame, image=logo_image, bg="black")
     logo_label.pack(anchor="nw", padx=10, pady=10)
-
-    # Create and style the "Name" input field
+ # Create and style the "Name" input field with a placeholder
     name_entry = ttk.Entry(frame, font=("Helvetica", 16), width=80)
-    name_entry.insert(0, "Name")
+    name_entry.insert(0, "Name")  # Set "Name" as a placeholder
     name_entry.pack(anchor="center", ipadx=10, ipady=15, padx=10, pady=30)
     name_entry.config(style="Custom.TEntry")  # Apply custom styling
+    name_entry.bind("<FocusIn>", name_entry_click)  # Bind click event
+    name_entry.bind("<FocusOut>", name_entry_focus_out)  # Bind focus out event
 
-    # Create and style the "Email" input field
+    # Create and style the "Email" input field with a placeholder
     email_entry = ttk.Entry(frame, font=("Helvetica", 16), width=80)
-    email_entry.insert(0, "Email")
+    email_entry.insert(0, "Email")  # Set "Email" as a placeholder
     email_entry.pack(anchor="center", ipadx=10, ipady=15, padx=10, pady=30)
     email_entry.config(style="Custom.TEntry")  # Apply custom styling
+    email_entry.bind("<FocusIn>", email_entry_click)  # Bind click event
+    email_entry.bind("<FocusOut>", email_entry_focus_out)  # Bind focus out event
+
+    # Create a "Register" button as an image
 
     # Create a "Register" button as an image
     register_image = common.next_img
