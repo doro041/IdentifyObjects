9cb6e4d5c3dc0f23a32e0f00774e56e974bc9a7d
Fraser <ftlg987@gmail.com>
2023-09-11 22:55:40 +0100

    Database: now being updated

--- registrypage.py
+++ registrypage.py
@@ -6,6 +6,8 @@ import DB
 win = None
 name_entry = None
 email_entry = None
+error_label = None
+user_id = None
 
 # Function to handle the click event in the Name Entry
 def name_entry_click(event):
@@ -28,6 +30,7 @@ def email_entry_focus_out(event):
         email_entry.insert(0, "Email")  # Restore the placeholder tex
 
 def register_user():
+    global user_id
     # Get the user-entered data
     name = name_entry.get()
     email = email_entry.get()
@@ -37,15 +40,17 @@ def register_user():
     
     if user_id is not None:
         print(f"Registered User:\nName: {name}\nEmail: {email}\nUser ID: {user_id}")
+        common.release_win(win)
     else:
-        print("User already exists with the same username or email.")
+        print("User already exists with the same email.")
+        error_label["text"] = "User already exists with the same email"
     
-    common.release_win(win)
 
 def run(root):
     global win
     global name_entry
     global email_entry
+    global error_label
     win = root
 
     canvas = tk.Canvas(root, bg="black", borderwidth=0, highlightthickness=0)
@@ -75,6 +80,10 @@ def run(root):
     email_entry.bind("<FocusIn>", email_entry_click)  # Bind click event
     email_entry.bind("<FocusOut>", email_entry_focus_out)  # Bind focus out event
 
+    # init error label so errors can be displayed if necessary
+    error_label = tk.Label(frame, bg="black", fg="red")
+    error_label.pack(anchor="center", ipadx=10, ipady=15, padx=10, pady=30)
+
     # Create a "Register" button as an image
 
     # Create a "Register" button as an image
@@ -87,4 +96,5 @@ def run(root):
     style.configure("Custom.TEntry", borderwidth=1, relief="solid", background="#D9D9D9")
 
     root.mainloop()
+    return user_id
 
