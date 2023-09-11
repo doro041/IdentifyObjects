fba951edf5f9abd46ac8433144ad00ba57389ab8
Dolorious <doroteyastoyanova555@gmail.com>
2023-09-11 15:06:47 +0100

    Added Database for user , email

--- DB.py
+++ DB.py
@@ -1,25 +1,54 @@
 import sqlite3
 
-# Create or connect to the database file
-conn = sqlite3.connect("game_identify.db")
-cursor = conn.cursor()
+def create_tables():
+    # Create or connect to the database file
+    conn = sqlite3.connect("game_identify.db")
+    cursor = conn.cursor()
 
-# Users table
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                 id INTEGER PRIMARY KEY,
-                 username TEXT UNIQUE NOT NULL,
-                 email TEXT UNIQUE NOT NULL
-                 )''')
+    # Users table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                     id INTEGER PRIMARY KEY,
+                     username TEXT UNIQUE NOT NULL,
+                     email TEXT UNIQUE NOT NULL
+                     )''')
 
-# Dashboard table
-cursor.execute('''CREATE TABLE IF NOT EXISTS game_scores (
-                 id INTEGER PRIMARY KEY,
-                 user_id INTEGER,
-                 score INTEGER,
-                 timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-                 FOREIGN KEY (user_id) REFERENCES users(id)
-                 )''')
+    # Dashboard table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS game_scores (
+                     id INTEGER PRIMARY KEY,
+                     user_id INTEGER,
+                     score INTEGER,
+                     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                     FOREIGN KEY (user_id) REFERENCES users(id)
+                     )''')
+
+    conn.commit()
+    conn.close()
 
 
-conn.commit()
-conn.close()
+def register_user(username, email):
+    conn = sqlite3.connect("game_identify.db")
+    cursor = conn.cursor()
+
+    try:
+        # Attempt to insert a new user into the 'users' table
+        cursor.execute("INSERT INTO users (username, email) VALUES (?, ?)", (username, email))
+        conn.commit()
+        user_id = cursor.lastrowid  # Get the ID of the newly inserted user
+        conn.close()
+        return user_id
+    except sqlite3.IntegrityError:
+        # Handle the case where a user with the same username or email already exists
+        conn.close()
+        return None
+
+def record_game_score(user_id, score):
+    conn = sqlite3.connect("game_identify.db")
+    cursor = conn.cursor()
+
+    # Insert the game score into the 'game_scores' table
+    cursor.execute("INSERT INTO game_scores (user_id, score) VALUES (?, ?)", (user_id, score))
+    conn.commit()
+    conn.close()
+
+# Create the necessary tables if they don't exist
+create_tables()
--- registrypage.py
+++ registrypage.py
@@ -1,26 +1,26 @@
 import tkinter as tk
 from tkinter import ttk
-
 import common
+import DB  
 
 win = None
 name_entry = None
 email_entry = None
 
-name = None # might need name for later
-
 def register_user():
-    global name
     # Get the user-entered data
     name = name_entry.get()
     email = email_entry.get()
 
-    print(f"Registered User:\nName: {name}\nEmail: {email}")
-    # register user
-    # ...
+    # Check if the user already exists
+    user_id = DB.register_user(name, email)
+    
+    if user_id is not None:
+        print(f"Registered User:\nName: {name}\nEmail: {email}\nUser ID: {user_id}")
+    else:
+        print("User already exists with the same username or email.")
     
     common.release_win(win)
-
 
 def run(root):
     global win
@@ -30,9 +30,6 @@ def run(root):
 
     canvas = tk.Canvas(root, bg="black", borderwidth=0, highlightthickness=0)
     canvas.pack(fill=tk.BOTH, expand=True)
-
-
-
 
     # Create the frame for the main content
     frame = tk.Frame(canvas, bg="black")
@@ -46,24 +43,23 @@ def run(root):
     # Create and style the "Name" input field
     name_entry = ttk.Entry(frame, font=("Helvetica", 16), width=80)
     name_entry.insert(0, "Name")
-    name_entry.pack(anchor="center", ipadx=10, ipady=15, padx=10, pady=30) 
+    name_entry.pack(anchor="center", ipadx=10, ipady=15, padx=10, pady=30)
     name_entry.config(style="Custom.TEntry")  # Apply custom styling
 
     # Create and style the "Email" input field
     email_entry = ttk.Entry(frame, font=("Helvetica", 16), width=80)
     email_entry.insert(0, "Email")
-    email_entry.pack(anchor="center", ipadx=10, ipady=15, padx=10, pady=30) 
+    email_entry.pack(anchor="center", ipadx=10, ipady=15, padx=10, pady=30)
     email_entry.config(style="Custom.TEntry")  # Apply custom styling
 
-    # Create a "Register" button as an image 
+    # Create a "Register" button as an image
     register_image = common.next_img
     register_button = tk.Button(frame, image=register_image, command=register_user, relief="flat", borderwidth=0, highlightthickness=0, fg="white", bg="black", activebackground="black", activeforeground="white")
-    register_button.image = register_image  
+    register_button.image = register_image
     register_button.pack(padx=10, pady=(5, 20))
-
 
     style = ttk.Style()
     style.configure("Custom.TEntry", borderwidth=1, relief="solid", background="#D9D9D9")
 
     root.mainloop()
-    return name
+
